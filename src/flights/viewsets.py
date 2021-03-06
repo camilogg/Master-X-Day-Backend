fa2e65102499ffff304527bec0b475c1c8d74857
Adrian González <adrian.gonzalez@unillanos.edu.co>
2021-03-06 17:13:11 -0500

    some fixes in models and an extra action in viewset

--- src/flights/viewsets.py
+++ src/flights/viewsets.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from flights.models import Flight, City, FlightStatus, PassengerByFlight
@@ -24,6 +26,13 @@ class FlightStatusModelViewSet(ModelViewSet):
     serializer_class = FlightStatusSerializer
     queryset = FlightStatus.objects.all()
     permission_classes = (AllowAny,)
+
+    @action(detail=False)
+    def total(self, request):
+        response = {
+            'total_flights': Flight.objects.all().count()
+        }
+        return Response(response)
 
 
 class PassengerByFlightModelViewSet(ModelViewSet):
